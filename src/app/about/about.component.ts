3481a5ad1e221b94334867af3f3adc6512ab2aff
Ran Wei <rwei@mms.org>
2018-08-25 05:29:11 -0400

    layout ready

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatBottomSheet, MatBottomSheetRef, MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+  animal: string;
+  name: string;
+
+  constructor(private bottomSheet: MatBottomSheet, public dialog: MatDialog) { }
+
+  ngOnInit() {
+  }
+
+  openBottomSheet(): void {
+    this.bottomSheet.open(MyBottomSheetComponent);
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(MyDialogComponent, {
+      width: '250px',
+      data: { name: this.name, animal: this.animal }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
+
+}
+
+@Component({
+  selector: 'app-bottom-sheet',
+  templateUrl: 'bottom-sheet.html',
+})
+export class MyBottomSheetComponent {
+  constructor(private bottomSheetRef: MatBottomSheetRef<MyBottomSheetComponent>) { }
+
+  openLink(event: MouseEvent): void {
+    this.bottomSheetRef.dismiss();
+    event.preventDefault();
+  }
+}
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: 'dialog.html',
+})
+export class MyDialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<MyDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,70 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  MatButtonModule, MatCheckboxModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule,
+  MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule,
+  MatBottomSheetModule, MatDialogModule, MatInputModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { Routes, RouterModule } from '@angular/router';
+import { TableComponent } from './table/table.component';
+import { MyBottomSheetComponent, AboutComponent, MyDialogComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { FlexLayoutComponent } from './flex-layout/flex-layout.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'table', component: TableComponent },
+  { path: 'flex-layout', component: FlexLayoutComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    AboutComponent,
+    HomeComponent,
+    LoginComponent,
+    FlexLayoutComponent,
+    MyBottomSheetComponent,
+    MyDialogComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    RouterModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatBottomSheetModule,
+    MatDialogModule,
+    MatInputModule
   ],
+  entryComponents: [MyBottomSheetComponent, MyDialogComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
