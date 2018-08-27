183abf495a5953af5909f0a0e65fcd0e7756a1c6
Ran Wei <rwei@mms.org>
2018-08-27 10:54:11 -0400

    Clean up some code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Observable, Subscription } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { AuthService } from './services/auth.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,14 +9,6 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'Angular Starter App';
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
-
-  constructor(private router: Router, private breakpointObserver: BreakpointObserver, private auth: AuthService) {
-    this.auth.stateChange$.subscribe((state) => {
-
-    });
+  constructor(private auth: AuthService) {
   }
 }
